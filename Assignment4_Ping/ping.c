232661a356c11479132b05e7718763777285e3ce
Mor <toxice290@gmail.com>
2026-01-02 17:54:07 +0200

    setting up the ground work for the ping program

--- Assignment4_Ping/ping.c
+++ Assignment4_Ping/ping.c
@@ -10,4 +10,37 @@
 #include <errno.h>
 #include <signal.h>
 
-#define PAKCET_SIZE 64
+
+/**
+ * @brief this is a PING program, similar to the ping we know and love,
+ * the only difference is this Ping allows to use 3 flags that did not exist in the origianl:
+ * ping -a <ip_addr> : recives the ip address
+ * ping -a -c <ip_addr> : pings the ip address up to c times
+ * ping -a -c -f <ip_addr> : 
+ * 
+ * use case example:
+ * ============================================================
+ *  ./ping -c 4 -a 8.8.8.8                                    =
+ *   Pinging 8.8.8.8 with 64 bytes of data:                   =
+    64 bytes from 8.8.8.8: icmp_seq=1 ttl=117 time=5.980ms    =  
+    64 bytes from 8.8.8.8: icmp_seq=2 ttl=117 time=6.830ms    =
+    64 bytes from 8.8.8.8: icmp_seq=3 ttl=117 time=6.970ms    =  
+    64 bytes from 8.8.8.8: icmp_seq=4 ttl=117 time=8.450ms    = 
+===============================================================
+ */
+
+#define PAKCET_SIZE 64 // packet size in bytes
+#define TIMEOUT_SEC 10 // timeout as specifed in the assignment PDF
+
+// creating the packet's struct
+struct ping_packet {
+    struct icmphdr icmp_header;
+    char payload[PAKCET_SIZE - sizeof(struct icmphdr)];
+};
+
+
+int main(int argc, char *argv[]) {
+    // here the ping will be executed...
+    return 0;
+}
+
---
+++ Assignment4_Ping/ping.h
@@ -0,0 +1,5 @@
+
+/**
+ * @brief calculating checksum before sending a packet
+ */
+unsigned short in_checksum(unsigned short *addr, int count);
