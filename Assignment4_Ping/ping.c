3a04e1645c64344ba4a9eac081420aa809cbd691
Mor <toxice290@gmail.com>
2026-01-02 17:14:53 +0200

    initial commit

---
+++ Assignment4_Ping/ping.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <signal.h>
+
+#define PAKCET_SIZE 64
